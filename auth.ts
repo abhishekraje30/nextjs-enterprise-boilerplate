39be430e8a1f0d952e58f90671083416c368ecd7
Abhishek Bankar <abhishekraje30@gmail.com>
2024-08-13 11:19:06 +0530

    Next Auth middleware section added

--- auth.ts
+++ auth.ts
@@ -24,24 +24,66 @@ const config = {
     }),
   ],
   callbacks: {
+    /* Callbacks are asynchronous functions you can use to control what happens when an action is performed.
+    Callbacks allow you to implement access controls without a database and to integrate with external databases or APIs. */
+    async signIn({ user, account, profile, email, credentials }) {
+      /* Use the signIn() callback to control if a user is allowed to sign in. */
+      console.log({ user, account, profile, email, credentials })
+      const isAllowedToSignIn = true
+      if (isAllowedToSignIn) {
+        return true
+      } else {
+        // Return false to display a default error message
+        return false
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
+    },
+    async redirect({ url, baseUrl }) {
+      /* The redirect callback is called anytime the user is redirected to a callback URL (e.g. on signin or signout).
+      By default only URLs on the same URL as the site are allowed, you can use the redirect callback to customise that behaviour.
+
+      NOTE: The redirect callback may be invoked more than once in the same flow. */
+
+      console.log({ url, baseUrl })
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    },
     authorized({ request, auth }) {
+      // used in between signin and redirect
       const { pathname } = request.nextUrl
       if (pathname === "/middleware-example") return !!auth
       return true
     },
+
     jwt({ token, trigger, session, account }) {
-      if (trigger === "update") token.name = session.user.name
-      if (account?.provider === "keycloak") {
-        return { ...token, accessToken: account.access_token }
+      /* jwt callback is called whenever a JSON Web Token is created (i.e. at sign in) or updated (i.e whenever a session is accessed in the client).
+      Requests to /api/auth/signin, /api/auth/session and calls to getSession(), getServerSession(), useSession() will invoke this function, but only if you are using a JWT session.      
+      This method is not invoked when you persist sessions in a database.
+      The returned value will be encrypted, and it is stored in a cookie. */
+      console.log({ token, trigger, session, account })
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+        token.userId = account.userId
       }
       return token
     },
     async session({ session, token }) {
+      /* The session callback is called whenever a session is checked. By default, only a subset of the token is returned for increased security. If you want to make something available you added to the token (like access_token and user.id from above) via the jwt() callback, you have to explicitly forward it here to make it available to the client. you will get all the keys from jwt() return and you can filter or add more keys to the session object.
+
+      Invoked after e.g. getSession(), useSession(), /api/auth/session */
       if (token?.accessToken) {
         session.accessToken = token.accessToken
       }
       return session
     },
+  },
+  session: {
+    strategy: "jwt",
   },
   debug: process.env.NODE_ENV !== "production" ? true : false,
 } satisfies NextAuthConfig
--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,13 @@
-import { NextRequest, NextResponse } from "next/server"
-import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+import { auth } from "auth"
 
-export async function middleware(req: NextRequest) {
-  console.log(req)
-  const secret = process.env.AUTH_SECRET || ""
-  const token = await getToken({ req, secret, salt: "YOUR_SALT_VALUE" })
-  const { pathname } = req.nextUrl
-
+export default auth((req) => {
+  const pathname = req.nextUrl.pathname
   // Define protected routes
   const protectedRoutes = ["/protected"]
 
-  // Check if the user is authenticated
-  const isAuthenticated = !!token
+  // Check if the user is authenticated. If auth is null then the user is not authenticated
+  const isAuthenticated = !!req.auth
 
   // Redirect unauthenticated users trying to access protected routes
   if (protectedRoutes.includes(pathname) && !isAuthenticated) {
@@ -20,7 +16,7 @@ export async function middleware(req: NextRequest) {
 
   // Allow access to public routes
   return NextResponse.next()
-}
+})
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
